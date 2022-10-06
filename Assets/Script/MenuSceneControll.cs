b8fd709d15bb3d57b672bd36f6a2f114a7e6082d
LV <viveiros.lucas@hotmail.com>
2022-10-06 14:42:22 +0100

    Update MenuSceneControll.cs

--- Assets/Script/MenuSceneControll.cs
+++ Assets/Script/MenuSceneControll.cs
@@ -6,7 +6,7 @@ using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-//[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Collider))]
 public class MenuSceneControll : MonoBehaviour
 {
     #region publicRegion
@@ -14,9 +14,6 @@ public class MenuSceneControll : MonoBehaviour
     public GameObject scifiManager;
     public Canvas whiteCanvas;
     public Collider[] boxCollider = new Collider[5];
-
-    
-   
     #endregion
 
     #region privateRegion
@@ -49,121 +46,8 @@ public class MenuSceneControll : MonoBehaviour
             c.enabled = false;
         }
     }
-    /*
-    private void OnTriggerEnter(Collider col)
-    {
-        if (lookButton)
-        {
-            if (!col.CompareTag("ButtonActivator"))
-            {
-                return;
-            }
 
-            else if (col.CompareTag("ButtonActivator") && !pressedInProgress)
-            {
-                pressedInProgress = true;
-                               
-                if (col.name == "ActivatorVideoPT")
-                {
-                    vLanguage = 0;
-                    StartCoroutine(WaitButtonForVideo360());
-                }
-                else if (col.name == "ActivatorVideoES")
-                {
-                    vLanguage = 1;
-                    StartCoroutine(WaitButtonForVideo360());
-                }
-
-                else if (col.name == "ActivatorVideoEN")
-                {
-                    vLanguage = 2;
-                    StartCoroutine(WaitButtonForVideo360());
-                }
-                else if (col.name == "ActivatorVideoFR")
-                {
-                    vLanguage = 3;
-                    StartCoroutine(WaitButtonForVideo360());
-                }
-                else if (col.name == "ActivatorTOUR")
-                {
-                    vLanguage = 5;
-                    
-                    //SceneManager.LoadSceneAsync("TOUR");
-                }
-            }
-        }
-    }
-
-    private IEnumerator WaitButtonForVideo360()
-    {
-        yield return new WaitForSeconds(1);
-
-        if (videoManagerStreaming.GetCurrentVideo() == 0) //to avoid 2 button clicks on menu's loop 
-        {
-            if (vLanguage == 0)
-            {
-                cliquesPT.text = cliques[0].ToString();
-            }
-            else if (vLanguage == 1)
-            {
-                cliquesSP.text = cliques[1].ToString();
-            }
-            else if (vLanguage == 2)
-            {
-                cliquesEN.text = cliques[2].ToString();
-            }
-            else if (vLanguage == 3)
-            {
-                cliquesFR.text = cliques[3].ToString();
-            }
-
-            videoManagerStreaming.PlayFakeVideoDebug();
-
-        }
-
-    */
-
-    /*
-    buttonClick.Play(0);
-
-    videoManagerStreaming.PauseToggle();
-
-    foreach (Collider c in boxCollider)
-        c.enabled = false;
-
-    yield return new WaitForSeconds(1);
-
-    Renderer[] rs = scifiManager.GetComponentsInChildren<Renderer>();
-    foreach (Renderer r in rs)
-        r.enabled = false;
-
-    if (videoManagerStreaming.GetCurrentVideo() == 0) //to avoid 2 button clicks on menu's loop 
-    {
-        if (vLanguage == 0)
-        {
-            videoManagerStreaming.PlayVideoPortuguese();
-        }
-        else if (vLanguage == 1)
-        {
-            videoManagerStreaming.PlayVideoSpanish();
-        }
-        else if (vLanguage == 2)
-        {
-            videoManagerStreaming.PlayVideoEnglish();
-        }
-        else if (vLanguage == 3)
-        {
-            videoManagerStreaming.PlayVideoFrench();
-        }
-    }
-
-    */
-
-
-
-
-
-IEnumerator ShowInitTable()
+public IEnumerator ShowInitTable()
     {
         yield return new WaitForSecondsRealtime(30);
 
