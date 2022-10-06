ceb19b4fe46008200d5de7233e83d0ee89404c27
LV <viveiros.lucas@hotmail.com>
2022-10-06 14:44:32 +0100

    Update Dontdestroy.cs

--- Assets/Script/Dontdestroy.cs
+++ Assets/Script/Dontdestroy.cs
@@ -5,14 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class Dontdestroy : MonoBehaviour
 {
-    // Music won't stop ;)
     private Scene currentScene;
     private void Awake()
     {
-
-        //Create array with the number of game objects with tag Music
         GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
-        //If the lenght is more then one it means there's more sounds
         currentScene = SceneManager.GetActiveScene();
         if (objs.Length > 1)
         {
